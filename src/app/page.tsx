a8831e24c31976414e910be54c039fa0b4ca8414
Daniel Lopez <dlopez807@gmail.com>
2024-07-04 11:59:43 -0700

    update title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <Main className='flex flex-col p-4'>
       <div className='flex flex-grow flex-col items-center justify-center space-y-4'>
-        <Title>home</Title>
+        <Title>steal</Title>
       </div>
     </Main>
   )
