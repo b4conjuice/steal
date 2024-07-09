ffbd80f2f558cd8aef09f732840f7bbc332c0558
Daniel Lopez <dlopez807@gmail.com>
2024-07-08 19:46:13 -0700

    add basic structure of cstimer page, add icons

--- src/app/[page]/_components/CSTimer.tsx
+++ src/app/[page]/_components/CSTimer.tsx
@@ -1,8 +1,67 @@
+import {
+  AdjustmentsVerticalIcon,
+  ArrowUpTrayIcon,
+  Cog8ToothIcon,
+  CubeIcon,
+  CurrencyDollarIcon,
+  ListBulletIcon,
+} from '@heroicons/react/24/outline'
+
+const topItems = [
+  {
+    key: 'settings',
+    Icon: Cog8ToothIcon,
+  },
+  {
+    key: 'import-export',
+    Icon: ArrowUpTrayIcon,
+  },
+  {
+    key: 'scramble',
+    Icon: CubeIcon,
+  },
+]
+const bottomItems = [
+  {
+    key: 'solves',
+    Icon: ListBulletIcon,
+  },
+  {
+    key: 'donate',
+    Icon: CurrencyDollarIcon,
+  },
+  {
+    key: 'adjustments',
+    Icon: AdjustmentsVerticalIcon,
+  },
+]
+
+const scramble = `F2 D R2 F' D L' F U R2 L2 F2 U R2 B2 U' D' R2 U' F'`
+
 export default function CSTimerPage() {
   return (
-    <div className='container mx-auto flex max-w-screen-sm flex-col space-y-4 px-4 pt-8 md:px-0'>
-      <h1 className='text-3xl font-semibold'>CSTimer</h1>
-      <p>This is a CSTimer page</p>
+    <div className='flex flex-grow bg-black text-white'>
+      <aside className='w-1/4'>
+        <ul className='flex justify-between p-4'>
+          {topItems.map(item => (
+            <item.Icon key={item.key} className='h-8 w-8' />
+          ))}
+        </ul>
+        <h1 className='bg-[#3b5494] p-4 text-center text-3xl'>csTimer</h1>
+        <ul className='flex justify-between p-4'>
+          {bottomItems.map(item => (
+            <item.Icon key={item.key} className='h-8 w-8' />
+          ))}
+        </ul>
+      </aside>
+      <main className='flex-grow'>
+        <div className='bg-[#3b5494]'>{scramble}</div>
+        <div>20.20 (+1.42)</div>
+        <div>
+          <div>ao12: 18.35</div>
+          <div>ao1000: -</div>
+        </div>
+      </main>
     </div>
   )
 }
